4382d744bb37f981a5fb7be4f704d223bba6aa77
Andy Simmons <rocknswimmer116@gmail.com>
2022-12-22 17:10:02 -0500

    keeping main like deployed
    changed while figuring out branches on a fork

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(compression());
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.listen(3111);
+app.listen(3001);
 
 
-console.log('Listening on port 3111');
+console.log('Listening on port 3001');
