8c10579e2ccea185c298de6a9a8cfc3e5d02a9a9
Andy Simmons <rocknswimmer116@gmail.com>
2022-12-22 21:30:32 -0500

    should be set

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,16 @@ function App() {
   //This is going to be really ugly and bad practice for speed and easy since im using modals and accordians to do a silde show prezy within  a prezy cleaner loop without prezy commented out below
   return (
     <div className="App">
-      <h1 className='text_shadows'>Merry Christmas</h1>
+      <h1 className='text_shadows'>Merry Christmas Smiths!</h1>
       <br/>
       <button onClick={openModal}>OPEN CARD!</button>
       {openCard &&
       <Modal
         close={() => {setOpenCard(!openCard);}}
-        content={<div className='card'><div>Instead of just texting you "Merry Christmas" this year, I decided to send you a link to this generic card an internet stranger made! Hope a bit more thought and care goes into your New Year!</div>
+        content={<div className='card'><div>The cats say Meowy Christmas! Have a Jolly Time in VA. Hope this Brightens your day!</div>
         <br/>
-        <div className='signature'>Happy Holidays, Love</div>
-        <div className='signature'>Whoever Sent This Link</div>
+        <div className='signature'>Merry Christmas</div>
+        <div className='signature'>Andy</div>
         </div>}
       />}
     </div>
