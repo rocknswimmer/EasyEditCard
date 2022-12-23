e7958002033bc56ab14aa119d6feddd939ebbaf8
Andy Simmons <rocknswimmer116@gmail.com>
2022-12-22 20:57:45 -0500

    style changes made need to think of message

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,16 @@ function App() {
   //This is going to be really ugly and bad practice for speed and easy since im using modals and accordians to do a silde show prezy within  a prezy cleaner loop without prezy commented out below
   return (
     <div className="App">
-      <h1 className='text_shadows'>Merry Christmas</h1>
+      <h1 className='text_shadows'>Merry Christmas Mom!</h1>
       <br/>
       <button onClick={openModal}>OPEN CARD!</button>
       {openCard &&
       <Modal
         close={() => {setOpenCard(!openCard);}}
-        content={<div className='card'><div>Instead of just texting you "Merry Christmas" this year, I decided to send you a link to this generic card an internet stranger made! Hope a bit more thought and care goes into your New Year!</div>
+        content={<div className='card'><div>message</div>
         <br/>
-        <div className='signature'>Happy Holidays, Love</div>
-        <div className='signature'>Whoever Sent This Link</div>
+        <div className='signature'>Happy Holidays,</div>
+        <div className='signature'>Love Andy</div>
         </div>}
       />}
     </div>
